9957c451dd319767726584b29d621b65e1e8944a
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-03-04 11:33:10 +0900

    Used prisma for mutations

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,96 +1,46 @@
 import uuidv4 from "uuid/v4";
 
 const Mutation = {
-  createUser(parent, args, { db }, info) {
-    const emailTaken = db.users.some(user => user.email === args.data.email);
-
-    if (emailTaken) {
-      throw new Error("Email taken");
-    }
-
-    const user = {
-      id: uuidv4(),
-      ...args.data
-    };
-
-    db.users.push(user);
-
-    return user;
+  async createUser(parent, args, { prisma }, info) {
+    return prisma.mutation.createUser({ data: args.data }, info);
   },
-  deleteUser(parent, args, { db }, info) {
-    const userIndex = db.users.findIndex(user => user.id === args.id);
-
-    if (userIndex === -1) {
-      throw new Error("User not found");
-    }
-
-    const deletedUsers = db.users.splice(userIndex, 1);
-
-    db.posts = db.posts.filter(post => {
-      const match = post.author === args.id;
-
-      if (match) {
-        db.comments = db.comments.filter(comment => comment.post !== post.id);
-      }
-
-      return !match;
-    });
-    db.comments = db.comments.filter(comment => comment.author !== args.id);
-
-    return deletedUsers[0];
-  },
-  updateUser(parent, args, { db }, info) {
-    const { id, data } = args;
-    const user = db.users.find(user => user.id === id);
-
-    if (!user) {
-      throw new Error("User not found");
-    }
-
-    if (typeof data.email === "string") {
-      const emailTaken = db.users.some(user => user.email === data.email);
-
-      if (emailTaken) {
-        throw new Error("Email taken");
-      }
-
-      user.email = data.email;
-    }
-
-    if (typeof data.name === "string") {
-      user.name = data.name;
-    }
-
-    if (typeof data.age !== "undefined") {
-      user.age = data.age;
-    }
-
-    return user;
-  },
-  createPost(parent, args, { db, pubsub }, info) {
-    const userExists = db.users.some(user => user.id === args.data.author);
-
-    if (!userExists) {
-      throw new Error("User not found");
-    }
-
-    const post = {
-      id: uuidv4(),
-      ...args.data
-    };
-
-    db.posts.push(post);
-
-    if (args.data.published) {
-      pubsub.publish("post", {
-        post: {
-          mutation: "CREATED",
-          data: post
+  async deleteUser(parent, args, { prisma }, info) {
+    return prisma.mutation.deleteUser(
+      {
+        where: {
+          id: args.id
         }
-      });
-    }
-
-    return post;
+      },
+      info
+    );
+  },
+  async updateUser(parent, args, { prisma }, info) {
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: args.id
+        },
+        data: args.data
+      },
+      info
+    );
+  },
+  createPost(parent, args, { prisma }, info) {
+    return prisma.mutation.createPost(
+      {
+        data: {
+          title: args.data.title,
+          body: args.data.body,
+          published: args.data.published,
+          author: {
+            connect: {
+              id: args.data.author
+            }
+          }
+        }
+      },
+      info
+    );
   },
   deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
